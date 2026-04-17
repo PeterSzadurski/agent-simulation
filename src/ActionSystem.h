5e775c9fe27aa2b40f0bdc2f0c24d0f89727a093
Peter Szadurski <peter.szadurski@gmail.com>
2026-04-17 19:00:40 -0400

    store by reference

--- src/ActionSystem.h
+++ src/ActionSystem.h
@@ -8,9 +8,9 @@ class ActionSystem
 private:
     void gatherResource(int tick, std::shared_ptr<Entity> e, std::optional<Cords> &knowledgeTarget, entity_type resourceType, const std::string &logName);
     void useNoticeBoard(EntityManager &em, CKnowledge &knowledge);
-    MovementSystem m_movement;
-    DecisionSystem m_decision;
-    Grid m_grid;
+    MovementSystem &m_movement;
+    DecisionSystem &m_decision;
+    Grid &m_grid;
 
 public:
     ActionSystem(DecisionSystem &decision,
